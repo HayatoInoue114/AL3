c94d934d12da3eaca596c920d95d89b50904e1f2
k022g0087@g.neec.ac.jp <k022g0087@g.neec.ac.jp>
2023-11-14 10:07:40 +0900

    a

--- MT.h
+++ MT.h
@@ -1,8 +1,14 @@
 #pragma once
+#include <Matrix4x4.h>
 #include <Vector2.h>
 #include <Vector3.h>
-#include <Matrix4x4.h>
+#include <time.h>
 
+int GetRandom(int min, int max);
+
+// float GetRandom(float min, float max);
+
+float Clamp(float number, float min, float max);
 
 // 加算
 Vector3 Add(const Vector3& v1, const Vector3& v2);
@@ -18,8 +24,7 @@ float Length(const Vector3& v);
 Vector3 Normalize(const Vector3& v);
 
 static const int kColumnWidth = 60;
-//void VectorScreenPrintf(int x, int y, const Vector3& vector, const char* label);
-
+// void VectorScreenPrintf(int x, int y, const Vector3& vector, const char* label);
 
 // 行列の加法
 Matrix4x4 Add(const Matrix4x4& m1, const Matrix4x4& m2);
@@ -27,6 +32,10 @@ Matrix4x4 Add(const Matrix4x4& m1, const Matrix4x4& m2);
 Matrix4x4 Subtract(const Matrix4x4& m1, const Matrix4x4& m2);
 // 行列の積
 Matrix4x4 Multiply(const Matrix4x4& m1, const Matrix4x4& m2);
+
+float InverseNum(const Matrix4x4& m, int a, int b, int c, int d, int e, int f, int g, int h);
+float InverseNum2(const Matrix4x4& m, int a, int b, int c, int d, int e, int f);
+
 // 逆行列
 Matrix4x4 Inverse(const Matrix4x4& m);
 // 転置行列
@@ -44,7 +53,7 @@ Matrix4x4 MakeRotateZMatrix(float radian);
 // 3次元アフィン変換行列
 Matrix4x4 MakeAffineMatrix(const Vector3& scale, const Vector3& rotate, const Vector3& translate);
 
-//void MatrixScreenPrintf(int x, int y, const Matrix4x4& matrix);
+// void MatrixScreenPrintf(int x, int y, const Matrix4x4& matrix);
 
 // 1.透視投影行列
 Matrix4x4 MakePerspectiveFovMatrix(float fovY, float aspectRatio, float nearClip, float farClip);
@@ -57,3 +66,13 @@ Matrix4x4 MakeViewportMatrix(
 
 // クロス積
 Vector3 Cross(const Vector3& v1, const Vector3& v2);
+
+// ベクトル変換
+Vector3 TransformNormal(const Vector3& v, const Matrix4x4& m);
+
+// 行列とベクトルの掛け算
+Vector3 Multyply(const Vector3& v, const Matrix4x4& m);
+
+Vector3 Transform(const Vector3& num, const Matrix4x4& m);
+
+Vector3 Slerp(const Vector3& v1, const Vector3& v2, float t);
